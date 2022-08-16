1429be1e0cfe0747fcd838532adb26f8f0fe9073
ShiekhAmish <77582706+ShiekhAmish@users.noreply.github.com>
2022-08-16 02:26:00 -0700

    NewsHunter App Till vedio 35 with previous next
    NewsHunter App Till vedio 35 with previous next

--- src/components/News.js
+++ src/components/News.js
@@ -1,17 +1,85 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        I am a News Component....
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+   
+  static defaultProps= {
+    country:'us',
+    pageSize: 9,
+    category: 'general'
+  }
+  static defaultTypes= {
+    country:PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
+  capitalize=(string)=>{
 
-        <NewsItem/>
-        <NewsItem/>
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props){
+    super(props);
+    console.log("I am A Constructor");
+    this.state ={
+      articles: [],
+      loading: false,
+      page:1
+    }
+    document.title= `${this.capitalize(this.props.category)} - NewsHunter`;
+  }
+  async UpdateNews ()
+  {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=40d7b707060e4ef5974c5b6096a9d48c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch (url);
+    let parseedData = await data.json();
+    console.log(parseedData);
+    this.setState({articles: parseedData.articles,
+    totalResults:parseedData.totalResults,
+    loading:false
+    });
+  }
+  async componentDidMount()
+    {
+     this.UpdateNews();
+    }
+
+    HandlePrevClick= async()=>{
+      
+      this.setState({
+        page: this.state.page -1});
+        this.UpdateNews(); 
+    }
+    HandleNextClick= async()=>{
+        this.setState({
+          page: this.state.page +1});
+          this.UpdateNews(); 
+    
+  }
+  render() {
+    
+    return (
+      <div className='container my-3'>
+        <h2 className='text-center'>NewsHunter - Top {this.capitalize(this.props.category)} Headlines</h2>
+       {this.state.loading &&  <Spinner/>}
+        <div className="row my-3">
+        {!this.state.loading && this.state.articles.map((element)=>{
+          
+          return <div className="col-md-4" key={element.url}>
+          <NewsItem title={element.title? element.title.slice(0,50):""} description={element.description?element.description.slice(0,130): ""} ImageUrl={element.urlToImage} NewsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
+          </div>
+        
+        })}
+
+
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button type="button" disabled={this.state.page<=1} onClick={this.HandlePrevClick} className="btn btn-success">&larr; Previous</button>
+        <button type="button" disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} onClick={this.HandleNextClick} className="btn btn-success">Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+ 
+
   render() {
+    let {title,description, ImageUrl,NewsUrl, author, date,source}= this.props;
     return (
       <div>
-        I am A news Item2......
+    <div className="card" >
+    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
+    {source}
+    
+  </span>
+  <img src={ImageUrl? ImageUrl: 'https://i.ytimg.com/vi/-1o3TkbB_ww/hqdefault.jpg'} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <p className="card-text"><small className="text-muted">By {author? author :"Unknown"} on {new Date(date).toTimeString()}</small></p>
+    <a rel='noreferrer' href={NewsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+
+  </div>
+</div>
       </div>
     )
   }
